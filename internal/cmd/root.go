bf29a71a3f8eeefaef77d83397e713a380ed711f
Broken Pipe <navigaid@gmail.com>
2026-05-08 22:37:49 +0800

    boba: switch to boba.{Program,NewProgram} API

--- internal/cmd/root.go
+++ internal/cmd/root.go
@@ -18,6 +18,7 @@ import (
 	"strings"
 	"time"
 
+	"github.com/btwiuse/boba"
 	tea "charm.land/bubbletea/v2"
 	fang "charm.land/fang/v2"
 	"charm.land/lipgloss/v2"
@@ -123,7 +124,7 @@ crush --continue
 		model := ui.New(com, sessionID, continueLast)
 
 		var env uv.Environ = os.Environ()
-		program := tea.NewProgram(
+		program := boba.NewProgram(
 			model,
 			tea.WithEnvironment(env),
 			tea.WithContext(cmd.Context()),
